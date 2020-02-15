35b4c35d79e20435c9d270e237acb79bfd10a59a
BenceV <s1655552@sms.ed.ac.uk>
2020-02-15 12:43:10 +0000

    correct_decoder_try2

--- local_experiment_scripts/cifar10_arg_parsing_template.sh
+++ local_experiment_scripts/cifar10_arg_parsing_template.sh
@@ -6,5 +6,5 @@ export DATASET_DIR="data/"
 
 python train_evaluate_emnist_classification_system.py --batch_size 64 --continue_from_epoch -1 --seed 0 \
                                                       --num_epochs 1 --experiment_name 'cifar10_mixmatch_test_exp' \
-                                                      --use_gpu "True" --val-iteration 200 --dataset_name "cifar10"
+                                                      --use_gpu "False" --val-iteration 200 --dataset_name "cifar10"
 
