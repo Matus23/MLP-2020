219da64c3c7017ead147a94da589ab1f9a36cdc6
Matus Drgon <matusd@Matuss-MacBook-Pro.local>
2020-03-24 19:59:07 +0100

    vq-vae implementation

--- model_architectures.py
+++ model_architectures.py
@@ -249,6 +249,96 @@ class VAE_Encoder(nn.Module):
         out_pr = out_pr.view(-1, self.nChannels)
         return self.fc(out_pr), out_sampled, out_mean, out_std
 
+# code taken from: https://github.com/zalandoresearch/pytorch-vq-vae/blob/master/vq-vae.ipynb
+class VectorQuantizer(nn.Module):
+    def __init__(self, num_embeddings, embedding_dim, commitment_cost):
+        super(VectorQuantizer, self).__init__()
+
+        self._embedding_dim = embedding_dim
+        self._num_embeddings = num_embeddings
+
+        self._embedding = nn.Embedding(self._num_embeddings, self._embedding_dim)
+        self._embedding.weight.data.uniform_(-1 / self._num_embeddings, 1 / self._num_embeddings)
+        self._commitment_cost = commitment_cost
+
+    def forward(self, inputs):
+        # convert inputs from BCHW -> BHWC
+        inputs = inputs.permute(0, 2, 3, 1).contiguous()
+        input_shape = inputs.shape
+
+        # Flatten input
+        flat_input = inputs.view(-1, self._embedding_dim)
+
+        # Calculate distances
+        distances = (torch.sum(flat_input ** 2, dim=1, keepdim=True)
+                     + torch.sum(self._embedding.weight ** 2, dim=1)
+                     - 2 * torch.matmul(flat_input, self._embedding.weight.t()))
+
+        # Encoding
+        encoding_indices = torch.argmin(distances, dim=1).unsqueeze(1)
+        encodings = torch.zeros(encoding_indices.shape[0], self._num_embeddings, device=inputs.device)
+        encodings.scatter_(1, encoding_indices, 1)
+
+        # Quantize and unflatten
+        quantized = torch.matmul(encodings, self._embedding.weight).view(input_shape)
+
+        # Loss
+        e_latent_loss = F.mse_loss(quantized.detach(), inputs)
+        q_latent_loss = F.mse_loss(quantized, inputs.detach())
+        loss = q_latent_loss + self._commitment_cost * e_latent_loss
+
+        quantized = inputs + (quantized - inputs).detach()
+        avg_probs = torch.mean(encodings, dim=0)
+        # perplexity = torch.exp(-torch.sum(avg_probs * torch.log(avg_probs + 1e-10)))
+
+        # convert quantized from BHWC -> BCHW
+        return loss, quantized.permute(0, 3, 1, 2).contiguous()
+
+class VQVAE_Encoder(nn.Module):
+    def __init__(self, num_classes, depth=28, widen_factor=2, dropRate=0.0, num_channels=1):
+        super(VQVAE_Encoder, self).__init__()
+        nChannels = [16, 16*widen_factor, 32*widen_factor, 64*widen_factor]
+        assert((depth - 4) % 6 == 0)
+        n = (depth - 4) / 6
+        block = EncoderBasicBlock
+        
+        # 1st conv before any network block
+        self.conv1 = nn.Conv2d(in_channels=num_channels, out_channels=nChannels[0], kernel_size=3, stride=1,
+                               padding=1, bias=False)
+        # 1st block
+        self.block1 = EncoderNetworkBlock(n, nChannels[0], nChannels[1], block, 1, dropRate, activate_before_residual=True)
+        # 2nd block
+        self.block2 = EncoderNetworkBlock(n, nChannels[1], nChannels[2], block, 2, dropRate)
+        # 3rd block for mean
+        self.block3 = EncoderNetworkBlock(n, nChannels[2], nChannels[3], block, 2, dropRate)
+
+        self._vq_vae = VectorQuantizer(num_embeddings=512, embedding_dim=128,
+                                       commitment_cost=0.25)
+
+        self.bn1 = nn.BatchNorm2d(nChannels[3], momentum=0.001)
+        self.relu = nn.LeakyReLU(negative_slope=0.1, inplace=True)
+        self.fc = nn.Linear(nChannels[3], num_classes)
+        self.nChannels = nChannels[3]
+
+    def forward(self,x):
+        out = self.conv1(x)
+        out = self.block1(out)
+        out = self.block2(out)
+        out_re = self.block3(out)
+
+        loss, quantized = self._vq_vae(out_re)
+        
+        if args.vqorder == 'after':
+            out_pr = self.relu(self.bn1(quantized))
+
+        elif args.vqorder == 'before':
+            out_pr = self.relu(self.bn1(out_re))
+
+        out_pr = F.avg_pool2d(out_pr, 8)
+        out_pr = out_pr.view(-1, self.nChannels)
+
+        return self.fc(out_pr), loss, quantized
+
 class Decoder(nn.Module):
     def __init__(self, depth=28, widen_factor=2, dropRate=0.0, num_channels=1):
         super(Decoder, self).__init__()
@@ -328,5 +418,35 @@ class VAE(nn.Module):
         if ae:
             out = self.decoder(out)
             return pred, out, mu, logvar
+        else:
+            return pred
+
+
+class VQVAE(nn.Module):
+    def __init__(self, num_classes, depth=28, widen_factor=2, dropRate=0.0, num_channels=1):
+        super(VQVAE, self).__init__()
+        self.encoder = VQVAE_Encoder(num_classes, depth, widen_factor, dropRate, num_channels)
+        self.decoder = Decoder(depth, widen_factor, dropRate, num_channels)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                nn.init.xavier_normal_(m.weight.data)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.ConvTranspose2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+
+
+    def forward(self,x, ae=True):
+        pred, loss, quantized = self.encoder(x)
+        if ae:
+            out = self.decoder(quantized)
+            return pred, out, loss
         else:
             return pred
